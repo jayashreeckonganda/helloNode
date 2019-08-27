51b0d22bf4e85ff0ee2e8f8df0cd3736059788d7
jayashreeckonganda <40911150+jayashreeckonganda@users.noreply.github.com>
2019-08-27 19:57:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const port = 4200
 
 app.get('/', (req, res) => res.send('Hello Testing the Get Request-1002'))
 
+console.log("Working");
+
 app.listen(port, () => console.log(`Build Successfull!  ${port}!`))
